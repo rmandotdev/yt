bd91b019424c7125afc6fd0b74129db1f290a995
Roman A <dev@rman.dev>
2025-09-13 10:36:41 +0100

    Update dependencies, add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   output: "static",
   build: { format: "preserve" },
+  vite: { plugins: [tailwindcss()] },
+  site: "https://yt.rman.dev",
 });
