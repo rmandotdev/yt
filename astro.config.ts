47f52000ea6e5806484fd911129adb7b358108d2
Roman <dev@rman.dev>
2026-05-16 09:53:13 +0100

    chore: update dependencies (#5)
    * chore: add biome, format files
    
    * chore: update dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
